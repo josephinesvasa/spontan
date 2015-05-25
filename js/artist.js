e6849ea1defa40dc5a89881131f2cc1a34b31d33
josephinesvasa <jossan_60@hotmail.com>
2015-05-25 19:47:21 +0200

    jquery slide header menu

--- js/artist.js
+++ js/artist.js
@@ -4,11 +4,11 @@ function show_more(id, buttonid) {
     var checkid=('#menu'+buttonid).toString();
     var button=('#'+buttonid).toString();
     if($(checkid).css('display') == 'block'){
-        $(checkid).css('display', 'none');
+        $(checkid).slideUp(1000);
         $(button).val("+");
     }
     else{
-        $(checkid).css('display', 'block');
+        $(checkid).delay(550).slideDown(1000);
         $.get( "http://localhost/laravel_event/public/event/artists/"+id)
 
         .done(function( data ) {
